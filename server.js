09fed404b72733715106851829f33689f2913d54
Daksk1234 <95217969+Daksk1234@users.noreply.github.com>
2025-04-20 12:32:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,4 +31,4 @@ app.get('/api/test', (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(5002, () => console.log('Server running on port 5002'));
